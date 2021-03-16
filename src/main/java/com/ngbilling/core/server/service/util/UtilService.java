0fcf76de3b4dfc4021b9d98d196d39bbc37f1963
Rakesh <rakesh@ngecom.com>
2021-03-16 07:34:47 +0400

    EntityDefaults while creating entity

--- src/main/java/com/ngbilling/core/server/service/util/UtilService.java
+++ src/main/java/com/ngbilling/core/server/service/util/UtilService.java
@@ -1,5 +1,7 @@
 package com.ngbilling.core.server.service.util;
 
+import com.ngbilling.core.server.persistence.dto.user.CompanyDTO;
+import com.ngbilling.core.server.persistence.dto.user.UserDTO;
 import com.ngbilling.core.server.persistence.dto.util.CurrencyDTO;
 import com.ngbilling.core.server.persistence.dto.util.JbillingTable;
 import com.ngbilling.core.server.persistence.dto.util.LanguageDTO;
@@ -9,5 +11,6 @@ public interface UtilService {
 	public LanguageDTO findByLanguageCode(String code);
 	public CurrencyDTO findByCurrencyCode(String code);
 	public JbillingTable findByName(String tableName);
+	public void initEntityDefault(CompanyDTO company, UserDTO rootUser, LanguageDTO language);
 	
 }
